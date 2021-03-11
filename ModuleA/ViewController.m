171b9ca25e648decf41177c65d25825ab895c5f9
叶义峰 <912907352@qq.com>
2021-03-11 22:17:31 +0800

    1

--- ModuleA/ViewController.m
+++ ModuleA/ViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor cyanColor];
     // Do any additional setup after loading the view.
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
